7cfc93777fe5bfab965c440689965ec9bea78b32
Oleg Mirzaev <oleg.mir@gmail.com>
2019-08-14 10:38:01 +0300

    fs tes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
 fs.writeFileSync('notes.txt','This file was created by nodejs');
+fs.appendFileSync('notes.txt','\nappend test')
+fs.appendFileSync('notes.txt','\nappend test2')
