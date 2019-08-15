f76ad43c98cdaee0b0ea949751f2deafb460d611
Oleg Mirzaev <oleg.mir@gmail.com>
2019-08-15 15:31:38 +0300

    utils delete

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,2 @@
-//const add = require('./utils.js')
-//console.log(add(2,3))
 const getNotes = require('./notes.js')
 console.log(getNotes())
--- notes-app/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('----utils.js----')
-
-const name = 'my name'
-
-const add = function(a, b){
-    return a+b
-}
-
-module.exports = add
