b524549750bde3683240efe5755bbb0e2f593df2
Oleg Mirzaev <oleg.mir@gmail.com>
2019-08-18 15:21:11 +0300

    yargs usage

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,44 @@
 const getNotes=require('./notes.js')
 const chalk=require('chalk')
 const log = console.log
+const yargs = require('yargs')
 
-const command = process.argv[2]
+yargs.version('0.9')
 
-if(command ==='add'){
-    log('Adding note!')
-}else if(command==='remove'){
-    log('Removing note!')
-}
+//add command
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    handler: function(){
+        log('Adding a new note!')
+    }
+})
+
+//remove command
+yargs.command({
+    command:'remove',
+    describe:'Remove a Note',
+    handler: function(){
+        log('Removing the note!')
+    }
+})
+
+//list command
+yargs.command({
+    command:'list',
+    describe:'List all the notes',
+    handler: function(){
+        log('Listing all the notes')
+    }
+})
+
+//read command
+yargs.command({
+    command:'read',
+    describe:'Reading the note',
+    handler: function(){
+        log('Reading the note')
+    }
+})
+
+log(yargs.argv)
