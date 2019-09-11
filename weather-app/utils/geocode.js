138196b20ada9d81b0117f468f1bf67721bfa3f8
Oleg Mir <oleg.mir@gmail.com>
2019-09-11 22:40:41 +0300

    callbacks refactor

---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geoCode=(adress,callback)=>{
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(adress)+'.json?access_token=pk.eyJ1Ijoib2xlZ21pciIsImEiOiJjazBjb2c0anAxM3E5M2lwaTQxamh1aTdpIn0.YcNFKj-y2R6enrnt9EI9Rw&limit=1';
+
+    request({url:url, json:true}, (error, response)=>{
+        if(error){
+            callback('Unable to connect to weather service.', undefined)
+        }else if(response.body.features.length === 0){
+            callback('Error occured while fetching data.', undefined)
+        }else{
+             const longitude = response.body.features[0].center[1]
+             const latitude = response.body.features[0].center[0]
+             const location = response.body.features[0].place_name
+
+             callback(undefined, {latitude, longitude, location})
+        }
+    })
+}
+
+module.exports = geoCode
