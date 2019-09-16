3dc68b9752bedd1ddecdb9f01a39d00598292ba0
Oleg Mir <oleg.mir@gmail.com>
2019-09-16 21:53:06 +0300

    location input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,13 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Tel Aviv',(error, data)=>{
+const location = process.argv[2]
+
+if(!location){
+    return console.log('Please provide a location')
+}
+
+geocode(location,(error, data)=>{
 
     if(error){
         return console.log(error)
