2012adffe3e27132127db7e4f00bbed3b0ab5208
Oleg Mir <oleg.mir@gmail.com>
2019-09-16 21:40:37 +0300

    callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,11 +2,19 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 geocode('Tel Aviv',(error, data)=>{
-    console.log('Error: ', error)
-    console.log('Data: ', data)
+
+    if(error){
+        return console.log(error)
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+
+        if(error){
+            console.log(error)
+        }
+
+        console.log(data.location)
+        console.log(forecastData)
+    })
 })
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error: ', error)
-    console.log('Data: ', data)
-  })
