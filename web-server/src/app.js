7a4e590f6358669000a2d1c3dbed05b61323619a
Oleg Mir <oleg.mir@gmail.com>
2019-09-30 19:57:44 +0300

    express to load html files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,11 @@
+const path = require('path')
 const express = require('express')
 
 //express has 1 function that gets you the app
 const app = express()
+const publicDirectory = path.join(__dirname, '../public')
 
-//app.com
-app.get('',(req, res)=>{
-    res.send('<h1>Hello express!</h1>')
-})
-
-//app.com/help
-app.get('/help',(req, res)=>{
-    res.send({
-        name: 'Oleg',
-        birthdate: '1988'
-    })
-})
-
-//app.com/about
-app.get('/about',(req, res)=>{
-    res.send('<h1>About</h1>')
-})
+app.use(express.static(publicDirectory))
 
 app.get('/weather',(req, res)=>{
     res.send({
