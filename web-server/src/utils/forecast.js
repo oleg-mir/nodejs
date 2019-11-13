d58decc7479a177188e31b36bc9aabcbf54089ba
Oleg Mir <oleg.mir@gmail.com>
2019-11-13 20:02:49 +0200

    weather utils copy

---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast=(latitude, longitude, callback)=>{
+    const url = 'https://api.darksky.net/forecast/d19bd74d2b00420aa7809831e4252206/'+latitude+','+longitude+'?units=si&lang=en'
+
+    request({url, json: true}, (error, {body})=>{
+        if(error){
+            callback('Unable to connect to weather service.', undefined)
+        }else if(body.error){
+            callback(body.error, undefined)
+        }else{
+            const summary = body.daily.data[0].summary
+            const {temperature, precipProbability: precipation} = body.currently
+
+            callback(undefined, summary +' It is currently '+temperature+' degrees out. There is a '+precipation +'% chance of rain.')    
+        }   
+    })
+}
+
+module.exports = forecast
