8b8c67466e95d4dcb4c69491c22e7fd2b26f4a2d
Oleg Mir <oleg.mir@gmail.com>
2019-11-15 15:16:05 +0200

    fetch api usage. calling an API from client to server.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Cliens side javascript loaded')
+
+fetch('/weather?address=boston').then((response) => {
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forecastData)
+        }      
+    })
+})
