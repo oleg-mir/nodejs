5fac2c2ddcc6175130be24abf48ffac1eb6d5ec5
Oleg Mir <oleg.mir@gmail.com>
2019-11-19 22:11:50 +0200

    search form creation

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,19 @@
-console.log('Cliens side javascript loaded')
+const watherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
 
-fetch('/weather?address=boston').then((response) => {
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.location)
-            console.log(data.forecastData)
-        }      
+watherForm.addEventListener('submit', (e)=>{
+    //prevent default behaviour and refresh page
+    e.preventDefault()
+    const location = searchElement.value
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+            }else{
+                console.log(data.location)
+                console.log(data.forecastData)
+            }      
+        })
     })
 })
