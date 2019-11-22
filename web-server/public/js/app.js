64a92f69cf701ed7f430af5a5f3fd57e4fe8c2dd
Oleg Mir <oleg.mir@gmail.com>
2019-11-22 19:40:20 +0200

    Showing data in website and not console

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,24 @@
 const watherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
 
 watherForm.addEventListener('submit', (e)=>{
     //prevent default behaviour and refresh page
     e.preventDefault()
     const location = searchElement.value
 
+    messageOne.textContent = 'Loading Data...'
+    messageTwo.textContent = ''
+
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data)=>{
             if(data.error){
-                console.log(data.error)
+                messageOne.textContent = data.error
+                messageTwo.textContent = ''
             }else{
-                console.log(data.location)
-                console.log(data.forecastData)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecastData             
             }      
         })
     })
