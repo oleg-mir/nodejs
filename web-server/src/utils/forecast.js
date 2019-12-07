25fd8088df0b6560f53417a92910694f74bf67aa
Oleg Mir <oleg.mir@gmail.com>
2019-12-07 14:36:56 +0200

    low and high datils

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -10,9 +10,12 @@ const forecast=(latitude, longitude, callback)=>{
             callback(body.error, undefined)
         }else{
             const summary = body.daily.data[0].summary
+            const temperatureHigh = body.daily.data[0].temperatureHigh
+            const temperatureLow = body.daily.data[0].temperatureLow
+
             const {temperature, precipProbability: precipation} = body.currently
 
-            callback(undefined, summary +' It is currently '+temperature+' degrees out. There is a '+precipation +'% chance of rain.')    
+            callback(undefined, summary +' It is currently '+temperature+' degrees out. There is a '+precipation +'% chance of rain. Temerature High is '+temperatureHigh+'. '+'Temperature Low is '+temperatureLow)    
         }   
     })
 }
