0c3d5f220b157aa7c39b6641bbf3ee8e22024ed5
Oleg Mir <oleg.mir@gmail.com>
2020-02-08 19:06:40 +0200

    task manager

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -12,6 +12,18 @@ const User = mongoose.model('User',{
         required: true,
         trim: true
     },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength: 7,
+        validate(value){
+            if(value.toLowerCase().includes('password'))
+            {
+                throw new Error('password can not contain "password" word')
+            }
+        }
+    },
     email:{
         type: String,
         required: true,
@@ -36,31 +48,36 @@ const User = mongoose.model('User',{
 
 const Task = mongoose.model('Task',{
     description:{
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed:{
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-const me = new User({
-    name: "   oleg   ",
-    email: "OLEG@dsf.cpm   "
-})
-
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log('Error: '+error)
-})
-
-// const taskA = new Task({
-//     description: "Task A",
-//     completed: false
+// const me = new User({
+//     name: "   oleg   ",
+//     email: "OLEG@dsf.cpm   ",
+//     password:"123pas!sword"
 // })
 
-// taskA.save().then(()=>{
-//     console.log(taskA)
+// me.save().then(()=>{
+//     console.log(me)
 // }).catch((error)=>{
 //     console.log('Error: '+error)
 // })
+
+
+
+const task = new Task({
+    description: "Task B",
+})
+
+taskA.save().then(()=>{
+    console.log(task)
+}).catch((error)=>{
+    console.log('Error: '+error)
+})
